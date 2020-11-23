9c9dc63662b80db11be1e60d9154301b979a081e
Andreas <andreashoornstra@gmail.com>
2020-11-23 21:24:58 +0100

    Added some comments describing the program

--- Opdracht2/Opdracht2.go
+++ Opdracht2/Opdracht2.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("This is Print:")
+	// Printing with Print:
 	fmt.Print("Hello world!")
 	fmt.Print("Hello world!")
 	fmt.Println("\nThis is Println:")
+	// Printing with Println:
 	fmt.Println("Hello world!")
 	fmt.Println("Hello world!")
 }
