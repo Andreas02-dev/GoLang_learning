d0b23d0657aca0189ae638e58976acf54be3391c
Andreas <andreashoornstra@gmail.com>
2020-11-23 21:12:40 +0100

    Added source code for Opdracht2.go

---
+++ Opdracht2/Opdracht2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is Print:")
+	fmt.Print("Hello world!")
+	fmt.Print("Hello world!")
+	fmt.Println("\nThis is Println:")
+	fmt.Println("Hello world!")
+	fmt.Println("Hello world!")
+}
