be74490a85dc9798c28a0cb66109f8bc18a9e8e3
Andreas <andreashoornstra@gmail.com>
2020-11-26 13:37:32 +0100

    Added comments describing the program

--- Leertaak_2/Opdracht2/Opdracht2.go
+++ Leertaak_2/Opdracht2/Opdracht2.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
+	// Scan the 2 variables:
 	x := floatScanner("Vul uw eerste variabele in:")
 	y := floatScanner("Vul uw tweede variabele in:")
+	// Add the 2 variables and print the result:
 	fmt.Println(x + y)
 }
 
+// floatScanner prints the query and returns the float64 scanned result:
 func floatScanner(query string) (x float64) {
 	fmt.Println(query)
 	fmt.Scanln(&x)
