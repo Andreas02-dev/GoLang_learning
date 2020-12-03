9c243a186ce2f781d03873f19d5a691634d5b280
Andreas <andreashoornstra@gmail.com>
2020-12-03 20:57:14 +0100

    Added comments describing the program

--- Udemy_exercises/Ninja_level_5/Exercise_1/Exercise_1.go
+++ Udemy_exercises/Ninja_level_5/Exercise_1/Exercise_1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Data declaration part:
 type person struct {
 	firstName        string
 	lastName         string
@@ -11,21 +12,27 @@ type person struct {
 }
 
 func main() {
+	// Create a person:
 	p1 := person{
 		firstName:        "Andreas",
 		lastName:         "Hoornstra",
 		favoriteIcecream: []string{"Boba", "Fett"},
 	}
+	// Create a person:
 	p2 := person{
 		firstName:        "Anniek",
 		lastName:         "Aarsen",
 		favoriteIcecream: []string{"Fett", "Boba"},
 	}
+	// Print the first and last name of p1:
 	fmt.Println(p1.firstName, p1.lastName)
+	// Loop over the favorite ice cream slice of p1 and print the values:
 	for _, v := range p1.favoriteIcecream {
 		fmt.Printf("%v ", v)
 	}
+	// Print the first and last name of p2:
 	fmt.Printf("\n%v\n%v\n", p2.firstName, p2.lastName)
+	// Loop over the favorite ice cream slice of p2 and print the values:
 	for _, v := range p2.favoriteIcecream {
 		fmt.Printf("%v ", v)
 	}
