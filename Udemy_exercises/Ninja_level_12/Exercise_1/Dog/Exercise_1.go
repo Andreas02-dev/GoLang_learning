d9b056e60861277f673111ce0a28a68ba048b365
Andreas <andreashoornstra@gmail.com>
2020-12-10 22:02:54 +0100

    Added source code for exercise 1

---
+++ Udemy_exercises/Ninja_level_12/Exercise_1/Dog/Exercise_1.go
@@ -0,0 +1,6 @@
+package dog
+
+// Years takes humanYears (int) and returns the dogYears equivalent (int).
+func Years(humanYears int) (dogYears int) {
+	return humanYears * 7
+}
---
+++ Udemy_exercises/Ninja_level_12/Exercise_1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	dog "example/testmodule/repo/src/Udemy_exercises/Ninja_level_12/Exercise_1/Dog"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(dog.Years(1))
+}
