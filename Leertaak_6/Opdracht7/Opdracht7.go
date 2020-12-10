d4efd64b399e60f0657e9c65f771b04c4cdca60e
Andreas <andreashoornstra@gmail.com>
2020-12-10 13:55:00 +0100

    Added source code for assignment 7

---
+++ Leertaak_6/Opdracht7/Opdracht7.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// HandleFunc handles the function handleAanbevelingen:
+	http.HandleFunc("/aanbevelingen", handleAanbevelingen)
+	// HandleFUnc handles the function handleNieuweAanbeveling:
+	http.HandleFunc("/nieuwe-aanbeveling", handleNieuweAanbeveling)
+	// Listen and Serve the request:
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+// handleWelkom renders the html template:
+func handleAanbevelingen(response http.ResponseWriter, request *http.Request) {
+	render, _ := template.ParseFiles("aanbevelingen.html")
+	// Render the page:
+	render.Execute(response, nil)
+}
+
+func handleNieuweAanbeveling(response http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(request.FormValue("name"))
+	log.Println(request.FormValue("email"))
+	log.Println(request.FormValue("comment"))
+}
